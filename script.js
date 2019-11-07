d9a61b647649d67550dac1142cc95cf6ebe61f2f
gvic6506 <gnatiuk.maxim@gmail.com>
2019-11-07 20:05:39 +0200

    add style

---
+++ script.js
@@ -0,0 +1,15 @@
+const tasks = [
+  {
+    id: 1,
+    title: "learn  JS ",
+    text: "хоить на курси делать дз",
+    completed: false
+  },
+  {
+    id: 2,
+    title: "learn React ",
+    text: "хоить на курси делать дз",
+    completed: false
+  }
+];
+console.log("tom ");
